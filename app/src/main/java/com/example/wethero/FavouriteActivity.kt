ac88be45bcd4dcdf8010b00fd9e5ffd03b544cea
Asmaa <asmaaelkoprosy@gmail.com>
2023-04-07 02:39:07 +0200

    favourites UI

--- app/src/main/java/com/example/wethero/FavouriteActivity.kt
+++ app/src/main/java/com/example/wethero/FavouriteActivity.kt
@@ -9,3 +9,49 @@ class FavouriteActivity : AppCompatActivity() {
         setContentView(R.layout.activity_favourite)
     }
 }
+
+/*
+public class MainActivity extends AppCompatActivity {
+   SearchView searchView;
+   ListView listView;
+   ArrayList list;
+   ArrayAdapter adapter;
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+      searchView = findViewById(R.id.searchView);
+      listView = findViewById(R.id.listView);
+      list = new ArrayList<>();
+      list.add("Apple");
+      list.add("Banana");
+      list.add("Pineapple");
+      list.add("Orange");
+      list.add("Mango");
+      list.add("Grapes");
+      list.add("Lemon");
+      list.add("Melon");
+      list.add("Watermelon");
+      list.add("Papaya");
+      adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,list);
+      listView.setAdapter(adapter);
+      searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+         @Override
+         public boolean onQueryTextSubmit(String query) {
+            if(list.contains(query)){
+               adapter.getFilter().filter(query);
+            }else{
+               Toast.makeText(MainActivity.this, "No Match found",Toast.LENGTH_LONG).show();
+            }
+            return false;
+         }
+         @Override
+         public boolean onQueryTextChange(String newText) {
+            adapter.getFilter().filter(newText);
+            return false;
+         }
+      });
+   }
+}
+
+ */
---
+++ app/src/main/java/com/example/wethero/localdatabase/WeatherDao.kt
@@ -0,0 +1,2 @@
+package com.example.wethero.localdatabase
+
